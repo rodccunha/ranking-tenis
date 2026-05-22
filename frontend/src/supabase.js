b341fad9280fb1680b4881312dd50f7617a5f9e4
Rodrigo Cunha <rod.ccunha@gmail.com>
2026-05-22 15:44:24 -0400

    Configurando variaveis

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://esmxwokylfweaviwkbla.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzbXh3b2t5bGZ3ZWF2aXdrYmxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NzE0OTcsImV4cCI6MjA5NTA0NzQ5N30.6KvJVLrBISXBNmNfAuORHuJbIyuhifxNFru2ev8MZ-c'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+)
